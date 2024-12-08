22f8d0614af5c6a12a1a83d7f35afcda430c69ad
1031 <1031@ana.local>
2024-12-08 00:59:16 -0500

    feat(key-generatore): Create django sample project for generate rsa.

---
+++ generate_key.py
@@ -0,0 +1,29 @@
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography.hazmat.primitives import serialization
+
+private_key = rsa.generate_private_key(
+    public_exponent=65537,
+    key_size=2048
+)
+
+public_key = private_key.public_key()
+
+with open("rsa_keys/private_key.pem", "wb") as private_file:
+    private_file.write(
+        private_key.private_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PrivateFormat.PKCS8,
+            encryption_algorithm=serialization.NoEncryption()
+        )
+    )
+
+with open("rsa_keys/public_key.pem", "wb") as public_file:
+    public_file.write(
+        public_key.public_bytes(
+            encoding=serialization.Encoding.PEM,
+            format=serialization.PublicFormat.SubjectPublicKeyInfo
+        )
+    )
+
+
+print("RSA Keys generated successfully!")
